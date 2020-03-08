3351f7a91bc495c2747da9c1cfe5dee38066b3ff
Johnvinder <51428041+Johnvinder@users.noreply.github.com>
2020-03-07 23:41:45 -0500

    Login feature to prevent private token commits, Vinder joke command

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 const Discord = require('discord.js');
-const token = 'Njg2MDM5OTgxMjYwOTMxMDk1.XmRjdg.faMRgyKA6uIno3DAzjBwVSmKt_E'
-
+//const token = 'Njg2MDM5OTgxMjYwOTMxMDk1.XmRjdg.faMRgyKA6uIno3DAzjBwVSmKt_E'
 const client = new Discord.Client();
+var readline = require('readline');
+var token;
 
-client.on('message', (msg) => {
-    if (msg.content === 'Is johnvinder hot'){
-        msg.channel.send('YES');
-    }
-})
-
-client.on('ready', () => {
-    console.log('Bot is now connected');
-
-    //client.channels.find(y => y.name === "computers").send('yo');
+//so we can take in inputs using readline()
+var tokenInput = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
 });
 
-client.login(token);
+//unless the login step is done, do not try and connect to the discord client using an invalid token
+let login;
+
+tokenInput.question("Please provide us with a valid token value:", function(token) {
+    client.login(token);
+    login = true;
+    if (login == true){
+
+        //ask to tell a johnvinder joke
+        client.on('message', (msg) => {
+            if (msg.content === '!jokevinder'){
+                msg.channel.send('my name is jeff');
+            }
+        })
+
+        //tells us if bot is connected after client is ready
+        client.on('ready', () => {
+            console.log('Bot is now connected');
+        });
+    };
+});
