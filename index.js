7893c4341decdb1f1f32a592dddc52b4eef6c251
John L <51428041+TooManyJohns@users.noreply.github.com>
2020-03-12 01:32:04 -0400

    Fixed bug with catching errors while fetching game server information (#26)
    * Fixed bug with catching errors while fetching game server information
    
    * Changed KYS to kill, :)

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const cmdMal          =   new BotCmd('mal',        'Get information from MyAnime
 const cmdMalAnime     =     new BotCmd('anime',    'Find information on an anime. The next argument is the anime.');
 const cmdMalUser      =     new BotCmd('user',     'Find information on an user. The next argument is the user.');
 const cmdHelp         =   new BotCmd('help',       'Display information on how to utilize bot.');
-const cmdKys          =   new BotCmd('kys',        'Shuts down the VanillaBot.');
+const cmdKys          =   new BotCmd('kill',        'Shuts down the VanillaBot.');
 const cmdList = {
   cmd: cmdPrefix,
   next: [
@@ -133,36 +133,10 @@ client.on('message', (msg) => {
                     type: 'minecraft',
                     host: 'mc.datafire.dev', //or whatever server link it is..
                 }).then((state) => {
-                    msg.channel.send({
-                        embed: {
-                            color: 3066993,
-                            author: {
-                                name: "Minecraft",
-                                icon_url: "http://www.rw-designer.com/icon-image/5547-64x64x4.png",
-                            },
-                            title: "Mint Ice Cream",
-
-                            description: "hosted by ice cream team.",
-                            fields: [{
-                                name: "Server Info",
-                                value: (`**${state.connect}**`),
-                            }, {
-                                name: "Players",
-                                value: (`**${state.players.length}**`),
-                            }, {
-                                name: "Max Players",
-                                value: (`**${state.maxplayers}**`),
-                            }],
-
-                            footer: {
-                                icon_url: client.user.avatarURL,
-
-          
-                            }
-                        }
+                    msg.channel.send({embed: {color: 3066993,author: {name: "Minecraft",icon_url: "http://www.rw-designer.com/icon-image/5547-64x64x4.png",},title: state.name ,description: "Hosted by the Ice Cream Team.",fields: [{name: "Server Info",value: (`**${state.connect}**`),}, {name: "Players",value: (`**${state.players.length}**`),}, {name: "Max Players",value: (`**${state.maxplayers}**`),}],footer: {icon_url: client.user.avatarURL,}}});
+                    }).catch((error) => {
+                        msg.channel.send({embed: {color: 10038562,description: "Server is **offline**."}});
                     });
-
-                });
             } else if (args[0] === cmdSvr7D2D.cmd) {
                msg.channel.send("Getting fireless's server info....");
                 Gamedig.query({
@@ -170,32 +144,9 @@ client.on('message', (msg) => {
                     host: '7days.datafire.dev',
                     port: '26900',
                 }).then((state) => {
-                    msg.channel.send({
-                        embed: {
-                            color: 10038562,
-                            author: {
-                                name: "7days",
-                                icon_url: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/46b63d3c-ae67-464c-9a37-670829b2a157/d9yftdm-7deaf276-864d-4017-a3d8-ab45d39ce8f6.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzQ2YjYzZDNjLWFlNjctNDY0Yy05YTM3LTY3MDgyOWIyYTE1N1wvZDl5ZnRkbS03ZGVhZjI3Ni04NjRkLTQwMTctYTNkOC1hYjQ1ZDM5Y2U4ZjYucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.RBMbE0FZR08HwHjBNZmf2kHJSu5f01UZmsmf3C0P6P0",
-                            },
-                            title: "Strawberry Ice Cream",
-
-                            description: "hosted by ice cream team.",
-                            fields: [{
-                                name: "Server Info",
-                                value: (`**${state.connect}**`),
-                            }, {
-                                name: "Players",
-                                value: (`**${state.players.length}**`),
-                            }, {
-                                name: "Max Players",
-                                value: (`**${state.maxplayers}**`),
-                            }],
-                            footer: {
-                                icon_url: client.user.avatarURL,
-                            }
-                        }
-                    });
-
+                    msg.channel.send({embed: {color: 3066993,author: {name: "7days",icon_url: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/46b63d3c-ae67-464c-9a37-670829b2a157/d9yftdm-7deaf276-864d-4017-a3d8-ab45d39ce8f6.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzQ2YjYzZDNjLWFlNjctNDY0Yy05YTM3LTY3MDgyOWIyYTE1N1wvZDl5ZnRkbS03ZGVhZjI3Ni04NjRkLTQwMTctYTNkOC1hYjQ1ZDM5Y2U4ZjYucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.RBMbE0FZR08HwHjBNZmf2kHJSu5f01UZmsmf3C0P6P0",},title: "Strawberry Ice Cream",description: "Hosted by the Ice Cream Team.",fields: [{name: "Server Info",value: (`**${state.connect}**`),}, {name: "Players",value: (`**${state.players.length}**`),}, {name: "Max Players",value: (`**${state.maxplayers}**`),}],footer: {icon_url: client.user.avatarURL,}}});
+                }).catch((error) => {
+                    msg.channel.send({embed: {color: 10038562,description: "Server is **offline**."}});
                 });
             } else {
                 msg.channel.send({
@@ -206,21 +157,16 @@ client.on('message', (msg) => {
                             icon_url: "https://www.siriusdecisions.com/-/media/siriusdecisions/images/blog-images/2014/july/multiple-flavors.ashx?h=267&w=400&hash=4F34EB7E696FE7B84C5762AB30DDF636",
                         },
                         title: "Server Help",
-
-                        description: "view server info with !vb server command",
+                        description: "View server info with **!vb** server command",
                         fields: [{
                             name: "Servers",
                             value: "Minecraft\n7Days to die",
                         }, {
                             name: "Commands",
                             value: "mc\n7days",
-
                         }],
-
                         footer: {
                             icon_url: client.user.avatarURL,
-
-
                         }
                     }
                 });
@@ -253,7 +199,7 @@ client.on('message', (msg) => {
             msg.channel.send(generateHelp(cmdList, cmdHelpHeader2));
         // Kill the bot.
         } else if (command === cmdKys.cmd) {
-            msg.channel.send('Bye').then(m => {
+            msg.channel.send(':skull_crossbones: ...VanillaBot now shutting down... :skull_crossbones:').then(m => {
                 client.destroy();
             });
         }        
